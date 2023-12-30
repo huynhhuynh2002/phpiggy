15fa746aa2c1c37a057b55d997de789b2b9688a6
Nguyễn Văn Huỳnh <121120228+huynhhuynh2002@users.noreply.github.com>
2023-12-30 16:17:22 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+$app = include __DIR__ .'/../src/App/bootstrap.php';
+
+$app->run();
---
+++ src/Framework/App.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Framework;
+
+class App{
+    public function run(){
+        echo "Hello World";
+    }
+}
